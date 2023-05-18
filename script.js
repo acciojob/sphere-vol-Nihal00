db8d513921bc35295f1331c7b91abe48c2367696
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-18 12:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
     //Write your code here
-  
-} 
+	const radius = document.getElementById('radius');
+
+	let volume = (4/3) * Math.PI * (r*r*r);
+
+	return volume;
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
